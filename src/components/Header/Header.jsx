745cfeef236d5f1ead3f880653662c0ee2d97530
Danny Lambert <dlambert150293@gmail.com>
2021-02-11 09:00:35 +0000

    removed border around list

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ class Header extends Component {
         return (
         <div className={styles.header}>
         <h1>Welcome {this.props.user.title} {this.props.user.first} {this.props.user.last}</h1>
-            <h2>We have {this.props.tasks} things left to to do</h2>
+            <h2>We have {this.props.tasks} things left to do</h2>
         </div>
         );
     }
